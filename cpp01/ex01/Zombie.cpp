fc5827fddaaeda2829e907081c8d8e05ec267dda
ACH4Q <achaqmohammedamine1@gmail.com>
2025-08-16 05:23:35 +0100

    s3ib l7al

---
+++ cpp01/ex01/Zombie.cpp
@@ -0,0 +1,22 @@
+#include "Zombie.hpp"
+
+
+Zombie::Zombie() 
+{
+    return;
+}
+
+Zombie::~Zombie()
+{
+    std::cout << name << " Destroyed" << std::endl;
+}
+
+void Zombie::announce(void)
+{
+    std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+void Zombie::setname(std::string name)
+{
+    this->name = name;
+}
---
+++ cpp01/ex01/Zombie.hpp
@@ -0,0 +1,25 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie
+{
+private:
+    std::string name;
+public:
+
+void announce(void);
+void setname(std::string name);
+
+Zombie();
+~Zombie();
+
+};
+
+
+Zombie* newZombie( std::string name );
+void randomChump( std::string name );
+Zombie* zombieHorde( int N, std::string name );
+
+#endif
