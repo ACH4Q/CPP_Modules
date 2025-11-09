4e936a65bbb16061a87d828175ba326ad29266a1
ACH4Q <achaqmohammedamine1@gmail.com>
2025-11-09 17:44:01 +0100

    ex01

---
+++ cpp06/ex01/Serialize.hpp
@@ -0,0 +1,18 @@
+#ifndef SERIALIZE_HPP
+#define SERIALIZE_HPP
+
+
+class Serialize
+{
+private:
+    
+public:
+    Serialize();
+    ~Serialize();
+    uintptr_t serialize(Data* ptr);
+
+};
+
+
+
+#endif
