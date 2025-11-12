58adbe55ad2bebf8b2e5b144f2c93f891bc3ec9c
ACH4Q <achaqmohammedamine1@gmail.com>
2025-11-12 16:21:04 +0100

    ex01

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,14 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+template<typename T> T iter(T *addr,const int len,void (*F)(T &))
+{
+    if (arr = NULL || F = NULL;)
+        return ;
+    for (int i = 0; i < len; i++)
+    {
+        F(addr[i]);
+    }
+}
+
+#endif
