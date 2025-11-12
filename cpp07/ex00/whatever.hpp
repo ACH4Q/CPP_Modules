6be8ffabf4a01e112ca4f61770826793a38cee53
ACH4Q <achaqmohammedamine1@gmail.com>
2025-11-12 15:21:06 +0100

    ex00

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -1,9 +1,29 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
-template<typename T>  T ft_swap(T x, T y)
+template<typename T> void swap(T &x, T &y)
 {
+    T tmp;
 
+    tmp = x;
+    x = y;
+    y = tmp;
+}
+
+template<typename Y> Y min(Y &x,Y &y)
+{
+    if (x < y)
+        return x;
+    else
+        return y;
+}
+
+template<typename H> H max(H &x,H &y)
+{
+    if (x > y)
+        return x;
+    else
+        return y;
 }
 
 
