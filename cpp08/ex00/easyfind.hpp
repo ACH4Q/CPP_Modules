a97368734958da952a05cf9fb133bae1447be927
ACH4Q <achaqmohammedamine1@gmail.com>
2025-11-12 18:31:43 +0100

    cpp08 ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,15 +1,21 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-class easyfind
-{
-private:
-    
-public:
-    easyfind();
-    ~easyfind();
-};
+template<typename T>
 
+void easyfind(T &obj, int i)
+{
+    typename T::iterator it = std::find(obj.begin(), obj.end(), i);
+
+    if(it != obj.end())
+    {
+        std::cout << "Element found " << *it << std::endl;
+    }
+    else
+    {
+            throw std::runtime_error("Element not found ");
+    }
+}
 
 
 #endif
