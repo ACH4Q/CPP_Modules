af03be11d871e998005f9a9d8a1597938da7d6a8
ACH4Q <achaqmohammedamine1@gmail.com>
2025-11-14 23:06:50 +0100

    latest Correct verison of iimplenetin Span

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -1,58 +1,70 @@
 #include "Span.hpp"
-
+#include <limits>
 
 Span::Span() : N(0) {}
 
-Span::Span(unsigned int _N) : N(_N) {}
+Span::Span(unsigned int _N) : N(_N) 
+{
+    myVector.reserve(_N);
+}
 
 Span::Span(const Span& other)
 {
     this->N = other.N;
+    this->myVector = other.myVector;
 }
 
-Span Span::operator=(const Span& other)
+Span& Span::operator=(const Span& other)
 {
     if (this != &other)
     {
         this->N = other.N;
+        this->myVector = other.myVector;
     }
     return *this;
 }
 
 Span::~Span() {}
 
-void Span::addNumber(int i)
+void Span::addNumber(int number)
 {
-    if (static_cast<int>(myVector.size()) >= static_cast<int>(i))
+    if (myVector.size() >= N)
         throw SpanIsNotCorrect();
-    myVector.push_back(i);
+    
+    myVector.push_back(number);
 }
 
-const char * Span::SpanIsNotCorrect::what() const throw()
+const char* Span::SpanIsNotCorrect::what() const throw()
 {
-    return "Span Error: Not enough numbers or capacity is full";
+    return "Span Error: Not enough numbers to find a span or capacity is full.";
 }
 
 int Span::shortestSpan()
 {
     if (myVector.size() < 2)
         throw SpanIsNotCorrect();
+
     std::vector<int> sortedVec = myVector;
-    std::sort(myVector.begin(),myVector.end());
-    int mini_span = 2147483647;
+    std::sort(sortedVec.begin(), sortedVec.end());
+
+    long min_span = std::numeric_limits<long>::max();
+
     for (size_t i = 0; i < sortedVec.size() - 1; ++i)
     {
-        if (sortedVec[i + 1] - sortedVec[i] < mini_span)
-            mini_span = sortedVec[i + 1] - sortedVec[i];
+        long current_span = static_cast<long>(sortedVec[i + 1]) - static_cast<long>(sortedVec[i]);
+        if (current_span < min_span)
+            min_span = current_span;
     }
-    return mini_span; 
+    return static_cast<int>(min_span);
 }
 
 int Span::longestSpan()
 {
     if (myVector.size() < 2)
         throw SpanIsNotCorrect();
-    int min_value = *std::min_element(myVector.begin(),myVector.end());
-    int max_value = *std::max_element(myVector.begin(),myVector.end());
+
+    int min_value = *std::min_element(myVector.begin(), myVector.end());
+    int max_value = *std::max_element(myVector.begin(), myVector.end());
+    
     return (max_value - min_value);
 }
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -5,39 +5,39 @@
 #include <vector>
 #include <algorithm>
 #include <exception>
+#include <iterator>
 
 class Span
 {
 private:
     unsigned int N;
     std::vector<int> myVector;
+
 public:
     Span();
     Span(unsigned int _N);
     Span(const Span& other);
-    Span operator=(const Span& other);
+    Span& operator=(const Span& other);
     ~Span();
-    void addNumber(int i);
+
+    void addNumber(int number);
     int shortestSpan();
     int longestSpan();
 
     class SpanIsNotCorrect : public std::exception
     {
         public:
-            virtual const char * what() const throw();
+            virtual const char* what() const throw();
     };
-    
-    template<typename T>
-    void addnumbers(T begin, T end)
+
+    template<typename InputIterator>
+    void addNumbers(InputIterator begin, InputIterator end)
     {
-        if (std::distance(begin,end) + myVector + 1 > N)
+        if (myVector.size() + static_cast<size_t>(std::distance(begin, end)) > N)
             throw SpanIsNotCorrect();
-        myVector.insert(myVector.end(),begin, end);
-    }    
+        
+        myVector.insert(myVector.end(), begin, end);
+    }
 };
-
-
-
-
 
 #endif
