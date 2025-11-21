fcf45de6f7bc53149805e93c5a1dbe6aea8cd8f7
ACH4Q <achaqmohammedamine1@gmail.com>
2025-11-21 22:37:24 +0100

    loading data

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -28,3 +28,39 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other)
 }
 
 BitcoinExchange::~BitcoinExchange() {}
+
+void BitcoinExchange::loadDatabase()
+{
+    std::ifstream file("data.csv");
+    if (!file.is_open())
+        throw std::runtime_error("could not load database");
+    std::string line;
+    std::getline(file,line);
+    while (std::getline(file,line))
+    {
+        std::string date;
+        std::string ratestring;
+        std::stringstream ss(line);
+        double rate;
+
+        if (std::getline(ss,date,',') && ss >> rate)
+        {
+            this->_data[date] = rate;
+        }
+    }
+}
+
+void BitcoinExchange::validateDate(const std::string& date)
+{
+
+}
+
+void BitcoinExchange::validateValue(const std::string& valueStr)
+{
+
+}
+
+void BitcoinExchange::processInputFile(const std::string& filename)
+{
+
+}
